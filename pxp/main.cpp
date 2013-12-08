0531b964d99e3c891fbc68395a01a1cb157b27ac
andrew.pa <andrew.pa@outlook.com>
2013-12-07 16:43:29 -0800

    realtime raymarching + menger sponge! see iquilezles.org for much of the math

--- pxp/main.cpp
+++ pxp/main.cpp
@@ -7,6 +7,8 @@
 #include <render_texture.h>
 #include <states.h>
 #include <mesh.h>
+#include <camera.h>
+#include <input.h>
 
 mesh* create_ndc_quad(ComPtr<ID3D11Device> device)
 {
@@ -33,8 +35,23 @@ class pxp_app : public dx_app
 {
 	mesh* q;
 	shader s;
+	struct b
+	{
+		float t;
+		float3 cam_o;
+		float3 ww; float wwww;
+		float3 vv; float vvvv;
+		float3 uu; float uuuu;
+	};
+	constant_buffer<b> buf;
+	float3 camera_pos;
+	float3 camera_targ;
+	camera cam;
+
 public:
-	pxp_app() : dx_app(4, true)
+	pxp_app() : dx_app(4, true),
+		camera_pos(), camera_targ(5, 0, 0),
+		cam(float3(0,0,0), float3(1, 0, 0), 0.1f, 1000.f, to_radians(45.f))
 	{
 	}
 
@@ -43,6 +60,7 @@ public:
 		q = create_ndc_quad(device);
 		s = shader(device, read_data_from_package(L"vs.cso"), read_data_from_package(L"ps.cso"),
 			posnormtex_layout, _countof(posnormtex_layout));
+		buf = constant_buffer<b>(device, 0, { 0 });
 	}
 
 	void update(float t, float dt) override
@@ -51,13 +69,47 @@ public:
 		{
 			windowSizeChanged = false;
 		}
+		buf.data().t = t;
+		//camera_pos = 2.1f * float3(2.5f*sinf(0.25f*t), 1.f + 1.f*cosf(t*.13f), 2.5f*cosf(0.25f*t));
+		
+		const float spd = .75f;
+		if (keyboard::key_down('A'))
+			cam.strafe(-spd*dt);
+		else if (keyboard::key_down('D'))
+			cam.strafe(spd*dt);
+		if (keyboard::key_down('W'))
+			cam.forward(spd*dt);
+		else if (keyboard::key_down('S'))
+			cam.forward(-spd*dt);
+		if (keyboard::key_down('Q'))
+			cam.position().y += spd*dt;
+		else if (keyboard::key_down('E'))
+			cam.position().y -= spd*dt;
+
+		static const float d = XMConvertToRadians(1.f);
+		if (keyboard::key_down(VK_LEFT))
+			cam.rotate_worldY(-d);
+		else if (keyboard::key_down(VK_RIGHT))
+			cam.rotate_worldY(d);
+		if (keyboard::key_down(VK_UP))
+			cam.pitch(-d);
+		else if (keyboard::key_down(VK_DOWN))
+			cam.pitch(d);
+		
+		buf.data().cam_o = cam.position();
+		auto ww = buf.data().ww = cam.look(); //normalize(camera_targ - cam.position());
+		auto uu = buf.data().uu = normalize(cross(float3(0, 1, 0), ww));
+		buf.data().vv = normalize(cross(ww, uu));
+		buf.update(context);
 	}
 
 	void render(float t, float dt) override
 	{
 		dx_app::render(t, dt);
 		s.bind(context);
+		buf.bind(context, ShaderStage::Pixel);
 		q->draw(context);
+		buf.unbind(context, ShaderStage::Pixel);
 		s.unbind(context);
 	}
 };
--- pxp/ps.hlsl
+++ pxp/ps.hlsl
@@ -5,7 +5,142 @@ struct psinput
 	float3 normW : NORMAL;
 };
 
+cbuffer b : register(b0)
+{
+	float t;
+	float3 cam_o;
+	float3 ww; float wwww;
+	float3 vv; float vvvv;
+	float3 uu; float uuuu;
+};
+
+float maxcomp(in float3 p)
+{
+	return max(p.x, max(p.y, p.z));
+}
+float sdbox(float3 p, float3 b)
+{
+	float3 di = abs(p) - b;
+	float mc = maxcomp(di);
+	return min(mc, length(max(di, 0)));
+}
+
+float sdtorus(float3 p, float2 t)
+{
+	float2 q = float2(length(p.xz) - t.x, p.y);
+		return length(q) - t.y;
+}
+
+float smin(float a, float b, float k)
+{
+	float res = exp(-k*a) + exp(-k*b);
+	return -log(res) / k;
+}
+
+
+float3 mod(float3 x, float3 y)
+{
+	return x - y * floor(x / y);
+}
+
+float asdf(float3 p)
+{
+	float3 q = p;// mod(p, float3(4, 4, 4)) - .5*float3(4, 4, 4);
+		return sdtorus(q, float2(1.f, .25f));//smin(sdtorus(q, float2(1.f, .25f)), sdbox(q, float3(1.15f, 0.1f, 0.1f)), 32);
+}
+
+float4 map(in float3 p)
+{
+	//float cof = .1f*sin(t*.4f);
+	//return float4(asdf(p)+
+	//	sin(cof*p.x)*sin(cof*p.y)*sin(cof*p.z), 1, 0, 0);
+	float d = sdbox(p, float3(1.0f,1.0f,1.f));
+	float4 rs = float4(d, 1, 0, 0);
+
+	float s = 1;
+	for (int m = 0; m < 4; ++m)
+	{
+		float3 a = mod((p*s) , 2.f) - 1.f;
+		s *= 3.f;
+		float3 r = abs(1.f - 3.f * abs(a));
+			float da = max(r.x, r.y);
+		float db = max(r.y, r.z);
+		float dc = max(r.z, r.x);
+		float c = (min(da, min(db, dc)) - 1.0) / s;
+		if (c > d)
+		{
+			d = c;
+			rs = float4(d, min(rs.y, .2*da*db*dc), (1 + float(m)) / 4.0f, 0.f);
+		}
+	}
+	return rs;
+}
+
+float4 intersect(in float3 ro, in float3 rd)
+{
+	float t = 0.f;
+	for (int i = 0; i < 64; ++i)
+	{
+		float4 h = map(ro + rd*t);
+		if (h.x < 0.002)
+			return float4(t, h.y, h.z, h.w);
+		t += h.x;
+	}
+	return float4(-1.f,-1.f,-1.f,-1.f);
+}
+
+float3 normal(in float3 pos)
+{
+	float3 eps = float3(.001f, 0.f, 0.f);
+		float3 nor;
+	nor.x = map(pos + eps.xyy).x - map(pos - eps.xyy).x;
+	nor.y = map(pos + eps.yxy).x - map(pos - eps.yxy).x;
+	nor.z = map(pos + eps.yyx).x - map(pos - eps.yyx).x;
+	return normalize(nor);
+}
+
+float3 colormap(float mz)
+{
+	float3 col;
+	col.x = .6f + .4f*cos(5.4f + 6.2831f*mz);
+	col.y = .6f + .4f*cos(5.7f + 6.2831f*mz);
+	col.z = .6f + .4f*cos(2.0f + 6.2831f*mz);
+	return col;
+}
+
+float3 raycolor(in float3 ro, in float3 rd)
+{
+	float3 light = float3(1.f, .9f, .3f);
+
+	float3 col = float3(0, 0, 0);
+
+	float4 m = intersect(ro, rd);
+	if (m.x > 0.0f)
+	{
+		float3 pos = ro + m.x*rd;
+			float3 norm = normal(pos);
+		float shf;
+		float4 shadow = intersect(pos + light * 4, -light);
+		if (shadow.x > 0.01 && shadow.x < (4 - 0.01))
+			shf = 0.3f;
+		else
+			shf = 1.0f;
+		col = shf * colormap(m.z) * max(0.1, dot(light, norm));
+	}
+	return col;
+}
+
 float4 main(psinput i) : SV_TARGET
 {
-	return float4(i.texc, 0, 1);
+	float2 p = -1.f + 2.f *i.texc;
+	p.y *= -1;
+//	p.x *= 1.33f;
+	
+	float3 ro = cam_o; //2.1f*float3(2.5f*sin(0.25f*t), 1.0 + 1.0*cos(t*.13f), 2.5f*cos(0.25f*t));
+	//float3 ww = //normalize(float3(0, 0, 0) - ro);
+	//float3 uu = //normalize(cross(float3(0, 1, 0), ww));
+	//float3 vv = //normalize(cross(ww, uu));
+	float3 rd = normalize(p.x*uu + p.y*vv + 2.5*ww);
+
+	return float4(sqrt(raycolor(ro, rd)), 1);
 }
