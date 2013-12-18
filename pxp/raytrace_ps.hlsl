935df693ed1d713d0d9f46c355d12cc37514ec52
andrew.pa <andrew.pa@outlook.com>
2013-12-17 18:36:26 -0800

    started to fix BVH traversal

--- pxp/raytrace_ps.hlsl
+++ pxp/raytrace_ps.hlsl
@@ -112,6 +112,26 @@ bool hit_aabb(in float3 ro, in float3 rd, in float3 amin, in float3 amax)
 	return tmax >= tmin;
 }
 
+bool hit_aabb(in float3 ro, in float3 rd, in float3 amin, in float3 amax, out float t)
+{
+	if (inside_aabb(ro, amin, amax)) return true;
+	float3 rrd = 1.f / rd;
+		float tx1 = (amin.x - ro.x)*rrd.x;
+	float tx2 = (amax.x - ro.x)*rrd.x;
+	float tmin = min(tx1, tx2);
+	float tmax = max(tx1, tx2);
+	float ty1 = (amin.y - ro.y)*rrd.y;
+	float ty2 = (amax.y - ro.y)*rrd.y;
+	tmin = min(tmin, min(ty1, ty2));
+	tmax = max(tmax, max(ty1, ty2));
+	float tz1 = (amin.z - ro.z)*rrd.z;
+	float tz2 = (amin.z - ro.z)*rrd.z;
+	tmin = min(tmin, min(tz1, tz2));
+	tmax = max(tmax, max(tz1, tz2));
+	t = min(tmax, tmin);
+	return tmax >= tmin;
+}
+
 static const int stack_size = 8;
 
 bool scene_hit(in float3 ro, in float3 rd,
@@ -131,7 +151,7 @@ bool scene_hit(in float3 ro, in float3 rd,
 		{
 			//add world transform
 			uint nmesh_id = scene_triangles[n.right_ptr].mesh_id;
-		//	mesh m = scene_meshes[mesh_id];
+			//mesh m = scene_meshes[mesh_id];
 			stack_ptr++;
 			float nt = t;
 			float3 nnorm = norm;
@@ -145,14 +165,87 @@ bool scene_hit(in float3 ro, in float3 rd,
 				return true;
 			}
 		}
+		//else
+		//{
+		//	float left_t = 0, right_t = 0;
+		//	bool left_hit = hit_aabb(ro, rd, scene_tree[n.left_ptr].aabb_min, scene_tree[n.left_ptr].aabb_max, left_t);
+		//	bool right_hit = hit_aabb(ro, rd, scene_tree[n.right_ptr].aabb_min, scene_tree[n.right_ptr].aabb_max, right_t);
+		//	if (left_hit && right_hit)
+		//	{
+		//		if (left_t < right_t)
+		//		{
+		//			stack_ptr++;
+		//			stack[stack_ptr] = n.left_ptr;
+		//			stack_ptr--;
+		//		}
+		//		else
+		//		{
+		//			stack_ptr++;
+		//			stack[stack_ptr] = n.right_ptr;
+		//			stack_ptr--;
+		//		}
+		//	}
+		//	else
+		//	{
+		//		if (left_hit)
+		//		{
+		//			stack_ptr++;
+		//			stack[stack_ptr] = n.left_ptr;
+		//			stack_ptr--;
+		//		}
+		//		else if (right_hit)
+		//		{
+		//			stack_ptr++;
+		//			stack[stack_ptr] = n.right_ptr;
+		//			stack_ptr--;
+		//		}
+		//	}
+		//}
 		else if (hit_aabb(ro, rd, n.aabb_min, n.aabb_max))
 		{
-			stack_ptr++; //pop this node
-			stack[stack_ptr] = n.right_ptr;
-			stack_ptr--;
-			stack[stack_ptr] = n.left_ptr;
-			stack_ptr--;
+			stack_ptr++;
+			float left_t = -1;
+			float right_t = -1;
+			bool l = false;
+			l = hit_aabb(ro, rd, scene_tree[n.left_ptr].aabb_min, scene_tree[n.left_ptr].aabb_max, left_t);
+			bool r = false; 
+			r = hit_aabb(ro, rd, scene_tree[n.right_ptr].aabb_min, scene_tree[n.right_ptr].aabb_max, right_t);
+
+			if (l && r)
+			{
+				if (left_t < right_t)
+				{
+					stack[stack_ptr] = n.left_ptr;
+					stack_ptr--;
+				}
+				else
+				{
+					stack[stack_ptr] = n.right_ptr;
+					stack_ptr--;
+				}
+			}
+			else
+			{
+				if (l)
+				{
+					stack[stack_ptr] = n.left_ptr;
+					stack_ptr--;
+				}
+				else if (r)
+				{
+					stack[stack_ptr] = n.right_ptr;
+					stack_ptr--;
+				}
+			}
 		}
+		//else if (hit_aabb(ro, rd, n.aabb_min, n.aabb_max))
+		//{
+		//	stack_ptr++; //pop this node
+		//	stack[stack_ptr] = n.right_ptr;
+		//	stack_ptr--;
+		//	stack[stack_ptr] = n.left_ptr;
+		//	stack_ptr--;
+		//}
 	}
 	return false;
 }
@@ -170,7 +263,7 @@ float4 main(psinput i) : SV_TARGET
 	if (scene_hit(ro, rd, t, norm, tex, mid))
 	{
 		mesh m = scene_meshes[mid];
-		return float4(m.color.xyz, 1);
+		return float4(norm+m.color.xyz, 1);//m.color.xyz, 1);
 	}
 	else
 	{
